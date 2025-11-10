0af5f4e1f5f994b2afb573192e550912583e4bbf
Basmalamoustafa <basmalamoustafa55@gmail.com>
2025-11-10 22:02:12 +0200

    Add fun facts screen with all personality types

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/fun_fact_screen.dart'; // Import your screen
+import '/fun_fact_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fun Facts App',
       theme: ThemeData(
-        // To use the 'Inter' font, you must first
-        // 1. Download it from Google Fonts
-        // 2. Add it to an `assets/fonts/` folder
-        // 3. Declare it in your `pubspec.yaml`
         fontFamily: 'Inter', 
         primarySwatch: Colors.purple,
       ),
